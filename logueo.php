8a84dfd505cb20125d2945baf1323ada366fb25d
plataformaiafedigital <64176023+plataformaiafedigital@users.noreply.github.com>
2020-08-02 19:39:10 -0300

    Add files via upload

---
+++ logueo.php
@@ -0,0 +1,197 @@
+<?php
+
+
+    $password=$_POST["password"]; 
+
+         switch ($password) {
+                case "3150n":
+                   // window.alert("Hola AYALA ZOE, estás por enviar archvivos a tu carpeta de " + mensaje);
+                header("Location: https://drive.google.com/drive/folders/1SeS0EJYpFzTAUO6lC2GU9Rz05zIMXq6X?usp=sharing");
+                //    exit;
+                    break;
+                case "1917a":
+                    //window.alert("Hola BENITEZ MORENO ANDREA, estás por enviar archvivos a tu carpeta de " + mensaje);
+                header("Location: https://drive.google.com/drive/folders/1nsrDJ6KEUSejffkDZUCDGjYLB1Cg42Jy?usp=sharing");
+                    exit;
+                    break;
+                case "2346m" :
+                //    window.alert("Hola CABALLERO VERÓN MILAGROS, estás por enviar archvivos a tu carpeta de " + mensaje);
+                header("Location: https://drive.google.com/drive/folders/1m0zuzEXmEZDBO8GnMhZDCywUDjV7zS35?usp=sharing");
+                    exit;
+                    break; 
+                case "8927g" :
+                //window.alert("Hola CANTERO PRISCILA GERALDINE, estás por enviar archvivos a tu carpeta de " + mensaje);
+                header("Location: https://drive.google.com/drive/folders/1lMmsaO2acKKpyBEMHQi3U1PESkxNfSLH?usp=sharing");
+                    exit;
+                    break;
+                case "5076e" :
+                //    window.alert("Hola CARI LLANOS MELINA, estás por enviar archvivos a tu carpeta de " + mensaje);
+                header("Location: https://drive.google.com/drive/folders/1wMtbP0e8I_sPHHp40z19b68rkyshGo1T?usp=sharing");
+                    exit;
+                    break;
+                case "4319b" :
+                //    window.alert("Hola CASTILLO FLORES BIANCA, estás por enviar archvivos a tu carpeta de " + mensaje);
+                header("Location: https://drive.google.com/drive/folders/1FwTUe1f1sruizJQHzdY68zxDtjvEoZdA?usp=sharing");
+                    exit;
+                    break;
+                case "2805m" :
+                //    window.alert("Hola CERMELLI UMA MILENA, estás por enviar archvivos a tu carpeta de " + mensaje);
+                header("Location: https://drive.google.com/drive/folders/11pL59Mn6Zb6VormwtB6w4IQ1Cpab6AcB?usp=sharing");
+                    exit;
+                    break;
+                case "8690c" :
+                //    window.alert("Hola CISNERO BRISA CANDELA, estás por enviar archvivos a tu carpeta de " + mensaje);
+                header("Location: https://drive.google.com/drive/folders/1tANihEMLLHNXNQ4TbCJDk_plbJnmlEHU?usp=sharing");
+                    exit;
+                    break;
+                case "7146m" :
+                //    window.alert("Hola GÓMEZ MICHELLE ABIGAIL, estás por enviar archvivos a tu carpeta de " + mensaje);
+                header("Location: https://drive.google.com/drive/folders/1EDiQcR5s9SQNtnTeyo8-6sNzxGlXOksu?usp=sharing");
+                    exit;
+                    break;
+                case "4176a" :
+                //    window.alert("Hola MARCELLO KIARA ALEJANDRA, estás por enviar archvivos a tu carpeta de " + mensaje);
+                header("Location: https://drive.google.com/drive/folders/1joZK_x5XwSNZ7RkCdFALJm750onAwnV3?usp=sharing");
+                    exit;
+                    break;
+                case "4951g" :
+                //    window.alert("Hola NIEVA GUZMÁN MILAGROS, estás por enviar archvivos a tu carpeta de " + mensaje);
+                header("Location: https://drive.google.com/drive/folders/1FlKKD0BjWT8L5ByANud6KFQvBSUznSH7?usp=sharing");
+                    exit;
+                    break;
+                case "8259g" :
+                //    window.alert("Hola ORTIZ CANDELA GISELLE, estás por enviar archvivos a tu carpeta de " + mensaje);
+                header("Location: https://drive.google.com/drive/folders/12Kxrku4aG1ccQ7Uy6Y6eoomg4Ghv9U_K?usp=sharing");
+                    exit;
+                    break;
+                case "9378n" :
+                //    window.alert("Hola PÉREZ NADIA, estás por enviar archvivos a tu carpeta de " + mensaje);
+                header("Location: https://drive.google.com/drive/folders/1vMzg01wRz2s47HiugNmVm5QXrz6MJjGe?usp=sharing");
+                    exit;
+                    break;
+                case "5683m" :
+                //    window.alert("Hola RIZZO DÍAZ GUADALUPE, estás por enviar archvivos a tu carpeta de " + mensaje);
+                header("Location: https://drive.google.com/drive/folders/1XUTs4MsgzquwbEGl8qKhIn0k_QyMG8c5?usp=sharing");
+                    exit;
+                    break;
+                case "2791p" :
+                //    window.alert("Hola SUÁREZ PAULA VALENTINA, estás por enviar archvivos a tu carpeta de " + mensaje);
+                header("Location: https://drive.google.com/drive/folders/1fmbQTnjlnr54bGy020MYaLBL0QAUjwcN?usp=sharing");
+                    exit;
+                    break;
+                case "7095a" :
+                //    window.alert("Hola UYUQUIPA MORENA AYLÉN, estás por enviar archvivos a tu carpeta de " + mensaje);
+                header("Location: https://drive.google.com/drive/folders/1V8YDLYZ-RSdkpJ3Uj3KM42Nff467Sbes?usp=sharing");
+                    exit;
+                    break;
+                case "7340z" :
+                //    window.alert("Hola VERA ANTONIO ZAIRA, estás por enviar archvivos a tu carpeta de " + mensaje);
+                header("Location: https://drive.google.com/drive/folders/1BKPBP-5HOsKTM96sCkIYgDe_EqvaLG1Z?usp=sharing");
+                    exit;
+                    break;
+                case "7254v" :
+                //    window.alert("VERSACI MARIANA VICTORIA, estás por enviar archvivos a tu carpeta de " + mensaje);
+                header("Location: https://drive.google.com/drive/folders/1qZGn-po5F4oxkSs6Yp-Qzef2mv0yO-Ig?usp=sharing");
+                    exit;
+                    break;
+                case "5167g" :
+                //    window.alert("ALDERETE GONZALO, estás por enviar archvivos a tu carpeta de " + mensaje);
+                header("Location: https://drive.google.com/drive/folders/1ErJ7ZjShgkDFiCZ6AeofKWBXrySc-lpm?usp=sharing");
+                    exit;
+                    break;
+                case "4065j" :
+                //    window.alert("BRÍTEZ ROLANDO JOAQUIN, estás por enviar archvivos a tu carpeta de " + mensaje);
+                header("Location: https://drive.google.com/drive/folders/1NWPjoZnW_0ccYYpwQBxK1iQG3I8cyX6d?usp=sharing");
+                    exit;
+                    break;
+                case "8357d" :
+                //    window.alert("CÁCERES ALEXANDER DANIEL, estás por enviar archvivos a tu carpeta de " + mensaje);
+                header("Location: https://drive.google.com/drive/folders/1s3zYI0YWuS6KRlY_IB1RhYUZd2gTMn51?usp=sharing");
+                    exit;
+                    break;
+                case "8109a" :
+                //    window.alert("CALDERÓN TOLOZA AXEL, estás por enviar archvivos a tu carpeta de " + mensaje);
+                header("Location: https://drive.google.com/drive/folders/11Y6h14hTolNy-Y-FkWKhz_0JTmPRWMpP?usp=sharing");
+                    exit;
+                    break;
+                case "6238e" :
+                //    window.alert("CIOCOLETTO THOMÁS EMILIANO, estás por enviar archvivos a tu carpeta de " + mensaje);
+                header("Location: https://drive.google.com/drive/folders/1Qr8a43ve8IfWL8nfkPD0d2NffuKpW7yF?usp=sharing");
+                    exit;
+                    break;
+                case "6473j" :
+                //    window.alert("CORO CÁRDENAS JOEL, estás por enviar archvivos a tu carpeta de " + mensaje);
+                header("Location: https://drive.google.com/drive/folders/1lk4A7asuwRVvV5Wa6Iu1tqHOMKrESKna?usp=sharing");
+                    exit;
+                    break;
+                case "6579s" :
+                //    window.alert("CORONEL DUARTE SANTINO, estás por enviar archvivos a tu carpeta de " + mensaje);
+                header("Location: https://drive.google.com/drive/folders/1ZPpe4tTpJX4RMOC2w3VqmTeSmb2hYTlB?usp=sharing");
+                    exit;
+                    break;
+                case "3458n" :
+                //    window.alert("DELIONARI NORIEGA NICOLÁS, estás por enviar archvivos a tu carpeta de " + mensaje);
+                header("Location: https://drive.google.com/drive/folders/1uk0c5QI4AMQCu2xGZSqNVZbnY6aO5dbt?usp=sharing");
+                    exit;
+                    break;
+                case "1569a" :
+                //    window.alert("GAUNA ISAÍAS ARIEL, estás por enviar archvivos a tu carpeta de " + mensaje);
+                header("Location: https://drive.google.com/drive/folders/1jrfLwe5sBSY__u_AxP-f2V_BHUVOVQE4?usp=sharing");
+                    exit;
+                    break;
+                case "3619s" :
+                //    window.alert("GODOY SANTIAGO EZEQUIEL, estás por enviar archvivos a tu carpeta de " + mensaje);
+                header("Location: https://drive.google.com/drive/folders/1y7EkF0HOssXmJ97Z5UCZfN2bqKRTlSW3?usp=sharing");
+                    exit;
+                    break;
+                case "2473a" :
+                //      window.alert("GÓMEZ ALEXIS JAVIER, estás por enviar archvivos a tu carpeta de " + mensaje);
+                header("Location: https://drive.google.com/drive/folders/1Lk7DmObEfnl6H8v1VQwGX1S2pgNsOG6j?usp=sharing");
+                    exit;
+                    break;
+                case "7840m" :
+                //      window.alert("GÓMEZ NÚÑEZ MATÍAS, estás por enviar archvivos a tu carpeta de " + mensaje);
+                header("Location: https://drive.google.com/drive/folders/1Pm8Gf9m2wrEJ0RstDSykAg4fRw9Ck89c?usp=sharing");
+                    exit;
+                    break;
+                case "6907a" :
+                //      window.alert("JARDINES IGNACIO AGUSTÍN, estás por enviar archvivos a tu carpeta de " + mensaje);
+                header("Location: https://drive.google.com/drive/folders/1Cny9k-yj_Yhg7_01hJ4mnmxeBKcGIUHi?usp=sharing");
+                    exit;
+                    break;
+                case "1428j" :
+                //      window.alert("MAMANI ROJAS ÁNGEL, estás por enviar archvivos a tu carpeta de " + mensaje);
+                header("Location: https://drive.google.com/drive/folders/1sqOCyUuO-MnG51MiQx7zhm5Zhh6ZKTDs?usp=sharing");
+                    exit;
+                    break;
+                case "3845a" :
+                //      window.alert("ORTIZ AXEL IVÁN, estás por enviar archvivos a tu carpeta de " + mensaje);
+                header("Location: https://drive.google.com/drive/folders/15UHl6bqRPTYy_NfNVnR3u6ziqI6sUdDi?usp=sharing");
+                    exit;
+                    break;
+                case "1826m" :
+                //      window.alert("PÁEZ MATÍAS EZEQUIEL, estás por enviar archvivos a tu carpeta de " + mensaje);
+                header("Location: https://drive.google.com/drive/folders/1zrjF3dw5Pf45Ih_wSjudQe1PC7bEv4Nf?usp=sharing");
+                    exit;
+                    break;
+                case "9461b" :
+                //      window.alert("RÍOS BRUNO EZEQUIEL, estás por enviar archvivos a tu carpeta de " + mensaje);
+                header("Location: https://drive.google.com/drive/folders/11V3xdxajbPgYd14ylWwy2sn0Lh8H1QjC?usp=sharing");
+                    exit;
+                    break;
+                case "8034f" :
+                //      window.alert("SALAZAR FERNÁNDEZ BRIAN, estás por enviar archvivos a tu carpeta de " + mensaje);
+                header("Location: https://drive.google.com/drive/folders/1yFPAp6__a910AHUSPS_mkr9cgbvZWDZN?usp=sharing");
+                    exit;
+                    break;
+                case "1274j" :
+                //      window.alert("SERVÍN JOEL FACUNDO, estás por enviar archvivos a tu carpeta de " + mensaje);
+                header("Location: https://drive.google.com/drive/folders/1Hn0fekSwCip42sgkG7-lwreAFR1maD6e?usp=sharing");
+                    exit;
+                    break;
+                default : 
+                    echo "Has Ingresado una Contraseña Erronea";
+                    header("Location: index.html");
+                }
+                 
+?>﻿
